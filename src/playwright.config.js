ff8b233dc047c5b44ec1300d4f9a40169910eb1a
cm4all88 <cmm2ski@gmail.com>
2026-05-05 20:39:09 -0700

    update

--- src/playwright.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@playwright/test';
-
-export default defineConfig({
-  testDir: './tests',
-  use: {
-    headless: true,
-    viewport: { width: 390, height: 844 }, // iPhone size since it's a mobile app
-  },
-});
